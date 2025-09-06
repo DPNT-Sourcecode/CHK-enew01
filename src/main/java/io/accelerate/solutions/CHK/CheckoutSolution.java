55ca778d8b63151f4ef6bf14b90d6a8733b9f62b
sbx_user1051 <sbx_user1051@169.254.95.175>
2025-09-06 14:04:04 +0000

    Sat Sep 06 14:04:04 UTC 2025

--- src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
+++ src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
@@ -3,20 +3,65 @@ package io.accelerate.solutions.CHK;
 import io.accelerate.runner.SolutionNotImplementedException;
 
 public class CheckoutSolution {
-    public Integer checkout(String skus) {
-        Map<char, int> prices = new HashMap<>();
-        prices.put('A', 50);
+
+    public Map<char, int> prices = new HashMap<>();
+        prices.put("A", 50);
         prices.put('B', 30);
         prices.put('C', 20);
         prices.put('D', 15);
 
-        Map<char, String> specialOffers = new HashMap<>();
+     public   Map<char, String> specialOffers = new HashMap<>();
         specialOffers.put('A', "3A for 130");
         specialOffers.put('B', "2B for 45");
 
+    public Integer checkout(String skus) {
+
+
+
+
         charList = skus.toCharArray();
 
+        Map<char, int> count = new HashMap<>();
 
+        for(int i = 0; i < charList.length(); i++){
+            if(!prices.containsKey(charList[i])){
+                return -1;
+            }
+            if(count.containsKey(charList[i])){
+                count.put(char, count.get(charList[i])+1))
+            }else{
+                count.put(char, 1)
+            }
+        }
+
+        return calculatePrice(count);
+    }
+
+
+    public Integer calculatePrice(Map<char, int> count){
+        int totalCost = 0;
+
+        for(Map.Entry<char, int> entry : count.entrySet()){
+            char item = entry.getKey();
+            int countValue = entry.getValue();
+        }
+        if(specialOffers.get(item).equals("3A for 130")){
+            int offerPrice = 130;
+            int offerQuantity = 3;
+            totalCost += (countValue/offerQuantity) * offerPrice;
+            totalCost += (countValue%offerQuantity) * prices.get(item);
+        }else{
+            if(specialOffers.get(item).equals("2B for 45")){
+                int offerPrice = 45;
+                int offerQuantity = 2;
+                totalCost += (countValue/offerQuantity) * offerPrice;
+                totalCost += (countValue%offerQuantity) * prices.get(item);
+            }else{
+                totalCost += count * prices.get(item);
+            }
+        }
+        return totalCost;
     }
 }
+
 
